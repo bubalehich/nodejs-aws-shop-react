77d8f3e4004cbb8475fe3a510e470fcb6adff11f
Pchelnik, Yana [JACIE NON-J&J] <YPchelni@its.jnj.com>
2026-05-15 16:20:05 +0300

    feat(task-5): wire up import API for CSV upload page

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://mv9r6b0fml.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://96rbu86vd3.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
