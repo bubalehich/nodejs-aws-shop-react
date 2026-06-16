2fa7bac778eccf20fbdf10589086f62af36f4f32
Pchelnik, Yana [JACIE NON-J&J] <YPchelni@its.jnj.com>
2026-06-16 12:09:34 +0300

    fix(task-8): point apiPaths.order at Cart Service so checkout actually hits the API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 const API_PATHS = {
   product: "https://mv9r6b0fml.execute-api.us-east-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://ox3iacbar7.execute-api.us-east-1.amazonaws.com/prod/api/profile/cart",
   import: "https://etw2b0mtja.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://ox3iacbar7.execute-api.us-east-1.amazonaws.com/prod/api",
